968d9f8261d0b8b59b31be87cfc5c1e0746ffd63
Leonardo G <lgomes.leo@gmail.com>
2018-11-06 19:57:26 -0200

    odeio yasuo

--- data-mining.py
+++ data-mining.py
@@ -22,7 +22,6 @@ articles_dir = './txt/'
 filenames = [f for f in glob.glob(os.path.join(articles_dir, '*.txt'))]
 
 stemmer = PorterStemmer()
-
 # Lê o texto de um arquivo, fazendo os devidos pré-processamentos
 def get_data (filename):
     with open(filename, 'r', encoding='latin1') as f:
@@ -46,33 +45,83 @@ def get_data (filename):
 def get_class (filename):
     return filename.split('-')[0].split('\\')[1]
 
+print("lendo arquivos e executando pré-processamento...")
 # criação de um dataframe do pandas com uma coluna de temas e de textos
-dataset = pd.DataFrame({'themes': [get_class(f) for f in filenames],
+dataset = pd.DataFrame({'theme': [get_class(f) for f in filenames],
                         'data': [get_data(f) for f in filenames]})
 
+print("transformando textos em matriz termo-documento...")
 # transformação em matriz de termo/documento
 cv = CountVectorizer(max_features=1000, encoding='latin1')
-# gerador de 'folds' normalizadas para um k-fold cross-validation
-n_splits = 10
-skf = StratifiedKFold(n_splits=n_splits, shuffle=True)
-nb = GaussianNB()
 
 X = cv.fit_transform(dataset.data).toarray() # Bag of words de cada artigo
-y = dataset.iloc[:, 0] # Classificação dos artigos
+words = cv.get_feature_names() # palavras presentes em X
+y = dataset.theme # Classificação dos artigos
 
+# matriz de termo/documento com coluna de classe
+tdm = pd.concat([y, pd.DataFrame(X, columns=words)], axis=1)
+
+# determina a classe de um documento utilizando naive bayes
+def bayes(document, train_set, themes):
+    # dicionário que guarda as probabilidades de cada tema
+    probabilities = {}
+
+    # calcula a probabilidade condicional (parcial) de cada tema
+    for theme in themes:
+        # separa a porção do dataset associada ao tema atual
+        theme_set = train_set[train_set.theme == theme]
+        # calcula a probabilidade do tema atual (P(A))
+        theme_prob = (len(theme_set) / len(train_set))
+
+        # calcula a probabilidade condicional para cada palavra (P(B|A))
+        for word in theme_set.columns[1:]:
+            theme_prob *= ((len(theme_set[theme_set[word] == document[word]]) + 0.1) /
+                           (len(theme_set) + 0.1))
+
+        # guarda o valor no dicionário de probabilidades
+        probabilities[theme] = theme_prob
+
+    # retorna a entrada no dicionário
+    return max(probabilities, key=lambda k: probabilities[k])
+
+# gerador de 'folds' normalizadas para um k-fold cross-validation
+n_splits = 10 # quantidade de folds
+skf = StratifiedKFold(n_splits=n_splits, shuffle=True)
+
+print(f"realizando {n_splits}-fold cross-validation...")
+
+# inicialização da acurácia
 accuracy = 0
+# criação do conjunto de temas possíveis
+themes = list(set(tdm.theme))
+count = 0 # contagem de folds
 for train_index, test_index in skf.split(X, y):
     # separação entre teste e treinamento
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
+    train_set = tdm.iloc[train_index]
+    test_set = tdm.iloc[test_index]
 
-    nb.fit(X_train, y_train) # Fase de treinamento
+    print(f"fold {count} ({len(test_set)} itens):")
 
-    y_pred = nb.predict(X_test) # Fase de teste
+    correct_count = 0 # contagem de exemplos corretamente previstos
+    for index, doc in test_set.iterrows():
+        # calcula a predição para um exemplo do conjunto de tetes
+        pred = bayes(doc, train_set, themes)
+        # incrementa contagem de previsões corretas caso esta seja correta
+        correct = (pred == doc.theme)
+        correct_count += 1 if correct else 0
+        print(f"{'.' if correct else 'x'}", end='', flush=True)
 
-    cr = classification_report(y_test, y_pred)
-    print(cr)
+    correct_count /= len(test_set)
+    print(f"\nfold {count} accuracy: {correct_count}")
 
-    accuracy += accuracy_score(y_test, y_pred)
+    # adiciona à acurácia total a acurácia obtida neste fold
+    accuracy += correct_count / len(test_set)
 
-print (accuracy/n_splits)
+    # incrementa a contagem de folds
+    count += 1
+
+# divide a acurácia pela quantidade de folds, efetivamente calculando a média
+# das acurácias
+accuracy /= n_splits
+
+print (accuracy)
